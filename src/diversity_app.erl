adbb9760a62f9f8346800d72d36a01ecd072a81b
Raoul Hess <raoul@textalk.se>
2015-05-04 08:10:20 +0200

    Removed http server code from renderer.

--- src/diversity_app.erl
+++ src/diversity_app.erl
@@ -5,29 +5,6 @@
 -export([stop/1]).
 
 start(_Type, _Args) ->
-
-    Routes = [
-      {"/favicon.ico", diversity_resource_handler, favicon},
-      {"/sitemap.xml", diversity_resource_handler, sitemap},
-      {"/backend/ha/check.txt", diversity_status_check, []},
-      {"/", diversity_http, []}
-    ],
-    Dispatch = cowboy_router:compile([
-        {'_', Routes}
-    ]),
-    {ok, Port} = application:get_env('diversity', port),
-    lager:info("Diversity-erlang started. Listening to port ~p", [Port]),
-    case application:get_env(diversity, debug) of
-        {ok, true} ->
-            {ok, DebugUrl} = application:get_env(diversity, debug_url),
-            lager:info("-------------------------------------------------------------------------"),
-            lager:info("Debug mode active and will always use ~s", [DebugUrl]),
-            lager:info("-------------------------------------------------------------------------");
-        _ ->
-            ok
-    end,
-
-    cowboy:start_http(http, 100, [{port, Port}], [{env, [{dispatch, Dispatch}]}]),
     diversity_sup:start_link().
 
 stop(_State) ->
